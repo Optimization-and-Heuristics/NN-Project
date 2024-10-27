6a2e7346b2682010db7173c6484074157f00b6d5
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-10-27 12:56:20 +0000

    implemented AdamOptimizer and tested it

--- code/clases/Optimizer.py
+++ code/clases/Optimizer.py
@@ -1,7 +1,39 @@
-class SGDOptimizer:
+class Optimizer:
+    pass
+
+
+class SGDOptimizer(Optimizer):
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
 
     def update(self, model):
         for key, param in model.parameters.items():
             model.parameters[key] -= self.learning_rate * model.gradients[key]
+
+
+class AdamOptimizer(Optimizer):
+    def __init__(self, learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = {}
+        self.v = {}
+        self.t = 0
+
+    def update(self, model):
+        self.t += 1
+        for key, param in model.parameters.items():
+            if key not in self.m:
+                self.m[key] = 0
+                self.v[key] = 0
+
+            gradient_t = model.gradients[key]
+
+            self.m[key] = self.beta1 * self.m[key] + (1 - self.beta1) * gradient_t
+            self.v[key] = self.beta2 * self.v[key] + (1 - self.beta2) * (gradient_t ** 2)
+
+            m_hat = self.m[key] / (1 - self.beta1 ** self.t)
+            v_hat = self.v[key] / (1 - self.beta2 ** self.t)
+
+            model.parameters[key] -= self.learning_rate * m_hat / (v_hat ** 0.5 + self.epsilon)
