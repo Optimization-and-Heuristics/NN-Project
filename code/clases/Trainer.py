3d5d5424bd335061fcb7627c91d1731eb6334917
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-10-31 17:48:36 +0000

    added gathering info about validation subset results

--- code/clases/Trainer.py
+++ code/clases/Trainer.py
@@ -1,6 +1,7 @@
 from .Test import *
 import time
 
+
 class Trainer:
     def __init__(self, nn, optimizer, loss_function, loss_function_grad):
         self.nn = nn
@@ -12,9 +13,10 @@ class Trainer:
         print("-" * 68)
         loss = []
         acc = []
+        val_info = {"epoch": [], "loss": [], "acc": []}
         for epoch in range(1, epochs + 1):
             epoch_start_time = time.time()
-            
+
             predictions = self.nn.forward(X_train)
             loss.append(self.loss_function(Y_train, predictions))
             acc.append(evaluate(predictions, Y_train))
@@ -25,12 +27,15 @@ class Trainer:
                                       t=epoch)
             
             if epoch % print_every == 0 or epoch == 1:
-                val_pred = predict(self.nn,X_val)
+                val_pred = predict(self.nn, X_val)
                 val_acc = evaluate(val_pred, Y_val)
                 val_loss = self.loss_function(Y_val, self.nn.forward(X_val))
-                
+
                 print("| Epoch {:3.0f} | time: {:5.2f}s | val loss {:2.3f} | valid accuracy {:2.3f} |".format(
                     epoch, time.time() - epoch_start_time, val_loss, val_acc))
                 print("-" * 68)
-        
-        return loss, acc
+                val_info["epoch"].append(epoch)
+                val_info["loss"].append(val_loss)
+                val_info["acc"].append(val_acc)
+
+        return loss, acc, val_info
